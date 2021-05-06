ecab0bd443fa69276f72c5936d1536d948fecce9
t3x <t3xhnolainx@gmail.com>
2021-05-06 15:25:46 +0200

    Minor changes

--- src/api/friends.ts
+++ src/api/friends.ts
@@ -1,3 +1,3 @@
 import axios from 'axios';
 
-export const fetchFriends = async () => await axios.get('http://localhost:1337');
+export const fetchFriends = async (): Promise<Record<string, unknown>> => await axios.get('http://localhost:1337');
