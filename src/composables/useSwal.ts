7a71504018449cb72ec6ffc6a8c5f33ad30a8029
t3x <t3xhnolainx@gmail.com>
2021-05-06 17:56:08 +0200

    Default value for confirm button

--- src/composables/useSwal.ts
+++ src/composables/useSwal.ts
@@ -4,7 +4,7 @@ type Icons = "warning" | "success" | "info" | "question" | "error";
 type SwalType = (a: string, b: string, c: Icons, d: string) => Promise<SweetAlertResult<any>>
 
 const useSwal = () => {
-    const swal: SwalType = (title, text, icon, confirmButtonText) =>
+    const swal: SwalType = (title, text, icon, confirmButtonText = 'Close') =>
         Swal.fire({ title, text, icon, confirmButtonText });
     return { swal };
 };
