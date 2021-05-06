fd097799bcbd47d0a599f124cd15c955ac423aa6
t3x <t3xhnolainx@gmail.com>
2021-05-06 16:15:51 +0200

    Function type signatures

--- src/composables/useSwal.ts
+++ src/composables/useSwal.ts
@@ -1,7 +1,9 @@
-import Swal, { SweetAlertIcon } from 'sweetalert2';
+import Swal, { SweetAlertIcon, SweetAlertResult } from 'sweetalert2';
+
+type SwalType = (a: string, b: string, c: SweetAlertIcon, d: string) => Promise<SweetAlertResult<any>>
 
 const useSwal = () => {
-    const swal = (title: string, text: string, icon: SweetAlertIcon, confirmButtonText: string) =>
+    const swal: SwalType = (title, text, icon, confirmButtonText) =>
         Swal.fire({ title, text, icon, confirmButtonText });
     return { swal };
 };
