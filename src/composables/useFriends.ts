47be0b7420f0f4e55f3f073a0779fb109d1bfdcd
t3x <t3xhnolainx@gmail.com>
2021-05-07 11:17:02 +0200

    Fetching friends in a separate function

--- src/composables/useFriends.ts
+++ src/composables/useFriends.ts
@@ -5,13 +5,14 @@ import useSwal from '../composables/useSwal';
 const useFriends = () => {
     const { swal } = useSwal();
     const friends = ref({});
+    const fetchFriendsAction = async () => friends.value = (await fetchFriends()).data.data;
     const validPassword = new RegExp(/[0|+381]6\d[/|\s]?\d{2}[-|\s]?\d{2}[-|\s]?\d{2,3}/);
     const checkPassword = (pass: string) => validPassword.exec(pass) ? true : false;
     const resetFriends = () => friends.value = {};
-    const getFriends = async (pass: string) => {
+    const getFriends = (pass: string) => {
         resetFriends();
         checkPassword(pass) ?
-            friends.value = (await fetchFriends()).data.data :
+            fetchFriendsAction() :
             swal("Error!", "You've entered the wrong password!", "error", "Close");
     };
     return { friends, getFriends };
