5bc631d292659191fab2024c7c66c64d3def5f4f
t3x <t3xhnolainx@gmail.com>
2021-05-11 16:41:30 +0200

    Mocking axios

--- tests/useFriends.test.ts
+++ tests/useFriends.test.ts
@@ -1,15 +1,20 @@
+import axios from "../src/__mocks__/axios";
 import useFriends from "../src/composables/useFriends";
 
 const { friends, getFriends } = useFriends();
 const initialFriends = friends.value;
 
 describe("Checking the getFriends function...", () => {
+    beforeEach(() => {
+        axios.get.mockImplementationOnce(() => Promise.resolve({ data: { data: [ { firstName: "Marko" } ] } }));
+    });
+
     test("Friends initialized to {}", () => {
-        expect(initialFriends).toStrictEqual({});
+        expect(initialFriends).toStrictEqual({});;
     });
     test("getFriends executes corretly", async (done) => {
         await getFriends("+38166/63-30-051");
-        expect(friends.value).not.toEqual({});
+        expect(friends.value).toEqual([{firstName: "Marko"}]);
         done();
     });
     test("Fails with wrong password", async (done) => {
